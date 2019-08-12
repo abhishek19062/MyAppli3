e04c69345442a910c0457d2a6972cc489b44c1b5
bunny <arun19062@iiitd.ac.in>
2019-08-12 18:58:41 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+        public void sendMessage(View view)
+        {
+            Log.d("Mycode",  "printing");
+            Intent bc=new Intent(this, Main2Activity.class);
+            startActivity(bc);
+        }
+}
